f02419f01d16803822505d00c5fd7e9af83c3d6b
Wazir <ahsankamal25@gmail.com>
2023-03-31 17:57:47 +0500

    calc() and transform function in detail

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import Login from './Login/Login'
+// import Login from './Login/Login'
+import Practice from './Practice/Practice';
 
 function App() {
   return (
     <div className="App">
-      <Login/>
+      {/* <Login/> */}
+      <Practice/>
     </div>
   );
 }
---
+++ src/Practice/Practice.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Practice.css'
+
+const Practice = () => {
+  return (
+    <div className='main' >
+      <div className='box'>
+        Box
+      </div>
+    </div>
+  )
+}
+
+export default Practice
